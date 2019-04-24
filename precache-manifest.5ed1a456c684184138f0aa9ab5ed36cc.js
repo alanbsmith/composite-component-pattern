99d90bddb5a3fe4316a253ae7b25f87c13dddb20
Alan B Smith <a.bax.smith@gmail.com>
2019-04-24 07:20:24 -0600

    Updates

---
+++ precache-manifest.5ed1a456c684184138f0aa9ab5ed36cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67377792d36a4c3ef1e1dcd5aafa6e07",
+    "url": "/composite-component-pattern/index.html"
+  },
+  {
+    "revision": "452092090d641f0bcf01",
+    "url": "/composite-component-pattern/static/css/main.584a5270.chunk.css"
+  },
+  {
+    "revision": "19a7059aed37dfeed0ca",
+    "url": "/composite-component-pattern/static/js/2.a8867e91.chunk.js"
+  },
+  {
+    "revision": "452092090d641f0bcf01",
+    "url": "/composite-component-pattern/static/js/main.4a3e0cc0.chunk.js"
+  },
+  {
+    "revision": "1f895866f92d827d29c2",
+    "url": "/composite-component-pattern/static/js/runtime~main.e54a675a.js"
+  }
+]);
